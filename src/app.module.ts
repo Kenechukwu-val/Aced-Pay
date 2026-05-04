1e5a13afcb858da501190bed3615798072f6c1e9
Nwafor Valentine Kenechukwu <nwaforvalentinekenechukwu@Nwafors-MacBook-Pro.local>
2026-05-04 17:14:10 +0100

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PlansModule } from './plans/plans.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { PaymentsModule } from './payments/payments.module';
+
+@Module({
+  imports: [UsersModule, PlansModule, SubscriptionsModule, PaymentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
