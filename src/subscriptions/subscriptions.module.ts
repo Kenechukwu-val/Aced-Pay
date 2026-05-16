e13a2ab5a251bdbe2b819bfc42f3fcfce790fc43
Nwafor Valentine Kenechukwu <nwaforvalentinekenechukwu@Nwafors-MacBook-Pro.local>
2026-05-17 00:53:08 +0100

    modified the subscription modules

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PaystackService } from '../paystack/paystack.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [SubscriptionsController],
-  providers: [SubscriptionsService],
+  providers: [SubscriptionsService, PaystackService],
   exports: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
