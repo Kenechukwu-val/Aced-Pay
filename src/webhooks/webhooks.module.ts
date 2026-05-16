149b897d658ef0d6735a85f7d4810d5fdcf6396d
Nwafor Valentine Kenechukwu <nwaforvalentinekenechukwu@Nwafors-MacBook-Pro.local>
2026-05-17 00:44:58 +0100

    modified the webhook module

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { StripeWebhookController } from './paystack.webhook.controller';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { PaymentsModule } from '../payments/payments.module';
-import { StripeModule } from '../stripe/stripe.module';
+
 
 @Module({
-  imports: [SubscriptionsModule, PaymentsModule, StripeModule],
-  controllers: [StripeWebhookController],
+  imports: [SubscriptionsModule, PaymentsModule],
+  controllers: [],
 })
 export class WebhooksModule {}
